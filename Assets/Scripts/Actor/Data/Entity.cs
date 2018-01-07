2b2d53a850cca693cb164bc5eeb5bc8c88309999
Sam Streed <embersteelstreus@gmail.com>
2018-01-07 17:11:59 -0600

    Finished up some stuff with Ability. Moved on to Entity and Status.

--- Assets/Scripts/Actor/Data/Entity.cs
+++ Assets/Scripts/Actor/Data/Entity.cs
@@ -14,6 +14,23 @@ public class Entity : MonoBehaviour
 
 	#region INSTANCE_VARS
 
+	[Tooltip("Used to distinguish players from enemies")]
+	[SerializeField]
+	private Faction faction = Faction.neutral;
+
+	[Tooltip("How fast this Entity can move.")]
+	[SerializeField]
+	private int movespeed = 10;
+
+	private List<Ability> abilities;
+
+	public int abilityCount { get { return abilities.Count; } }
+	public int abilityCap { get { return abilities.Capacity; } }
+
+	public event AbilityChanged abilityAdded, abilityRemoved;
+	public event AbilitySwap abilitySwapped;
+	public event StatusChanged statusAdded, statusRemoved;
+	public event EntityGeneric died;
 	#endregion
 
 	#region STATIC_METHODS
@@ -22,5 +39,160 @@ public class Entity : MonoBehaviour
 
 	#region INSTANCE_METHODS
 
+	public void Awake()
+	{
+
+	}
+
+	public void Update()
+	{
+
+	}
+
+	public Faction getFaction()
+	{
+		return faction;
+	}
+
+	#region ABILITY_MANAGEMENT
+
+	public void addAbility(Ability a, int index = -1)
+	{
+		if (a == null)
+			return;
+
+		if (index >= 0 && index < abilities.Capacity)
+		{
+			if (abilities [index] != null && abilityRemoved != null)
+				abilityRemoved (abilities [index], index);
+			abilities [index] = a;
+		}
+		else
+			abilities.Add (a);
+
+		a.active = true;
+
+		if (abilityAdded != null)
+			abilityAdded (a, index);
+	}
+
+	/// <summary>
+	/// Removes the given ability from the ability list. Returns false if the removal
+	/// was unsuccessful.
+	/// </summary>
+	public bool removeAbility(Ability a)
+	{
+		for (int i = 0; i < abilities.Capacity; i++)
+		{
+			if (abilities [i].id == a)
+			{
+				removeAbility (i);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/// <summary>
+	/// Removes the given ability from the ability list. Returns false if the removal
+	/// was unsuccessful.
+	/// </summary>
+	public bool removeAbility(int index)
+	{
+		if (index < 0 || index >= abilities.Capacity)
+			return false;
+
+		if (abilities [index] == null)
+			return false;
+
+		Ability removed = abilities [index];
+		abilities [index] = null;
+		removed.active = false;
+
+		if (abilityRemoved != null)
+			abilityRemoved (removed, index);
+	}
+
+	/// <summary>
+	/// Swap an ability in the list for another. Returns the old ability.
+	/// </summary>
+	public Ability swapAbility(Ability a, int index)
+	{
+		Ability old = null;
+		try
+		{
+			old = abilities[index];
+			abilities[index] = a;
+			if(a != null)
+				a.active = true;
+			if(old != null)
+				old.active = false;
+		}
+		catch(System.IndexOutOfRangeException ioore)
+		{
+			Debug.LogError ("Encountered an error swapping abilities on " + gameObject.name
+				+ ".\n" + ioore.Message);
+			return null;
+		}
+
+		if (abilitySwapped != null)
+			abilitySwapped (a, old, index);
+
+		return old;
+	}
+
+	public Ability getAbility(int index)
+	{
+		if (index >= 0 && index < abilities.Capacity)
+			return abilities [index];
+		return null;
+	}
+	#endregion
+
+	public void addStatus(Status s)
+	{
+
+	}
+
+	public void removeStatus(Status s)
+	{
+
+	}
+
+	/* TODO Entity needs SeedBase
+	public SeedBase reap()
+	{
+
+	}
+
+	public void sow(SeedBase s)
+	{
+
+	}
+	*/
+
+	public void onDeath()
+	{
+		if (died != null)
+			died ();
+
+		//TODO maybe play death animation?
+
+		Destroy (gameObject); //TODO replace this with RegisteredObject's destroy eventually
+	}
+
+	#endregion
+
+	#region INTERNAL_TYPES
+
+	/// <summary>
+	/// Used to distinguish between different groups of Actors in gameplay
+	/// </summary>
+	public enum Faction { player, enemy, neutral }
+
+	public delegate void EntityGeneric();
+	public delegate void AbilityChanged(Ability a, int index = -1);
+	public delegate void AbilitySwap (Ability a, Ability old, int index);
+	public delegate void StatusChanged(Status s); 
 	#endregion
 }
--- Assets/Scripts/Actor/Data/Status.cs
+++ Assets/Scripts/Actor/Data/Status.cs
@@ -9,17 +9,110 @@ public class Status
 {
 	#region STATIC_VARS
 
+	// A repository for commonly used statuses
+	private static Dictionary<string, Status> repo;
+
+	// The latest ID not assigned to an status
+	private static int latestID = 0;
 	#endregion
 
 	#region INSTANCE_VARS
 
+	// The unique ID for this status
+	private int _id;
+	public int id { get { return _id; } }
+
+	// The displayed name of this status
+	public readonly string name;
+
+	// The base description of this status
+	public readonly string desc;
+
+	// The sprite displayed for this status
+	public readonly Sprite icon;
+
+	// The type of decay behavior this status has
+	public readonly DecayType decayType;
+
+	// The current number of applied stacks
+	private int _stacks;
+	public int stacks { get { return _stacks; } }
+
+	// The maximum number of stacks this status can accrue
+	public readonly int stacksMax;
+
+	// The remaining time this status will be applied before going through decay
+	private float _duration;
+	public float duration { get { return _duration; } }
+
+	// The inital duration of this status when it was applied
+	private float initDuration;
+
+	// The individual components that make up this status
+	private StatusComponent[] components;
 	#endregion
 
 	#region STATIC_METHODS
 
+	public static Status get(string name, float newDur = float.NaN)
+	{
+		Status s;
+		if (repo.TryGetValue (name, out s))
+		{
+			if (newDur != float.NaN)
+				return new Status (s, newDur);
+			return new Status (s);
+		}
+		return null;
+	}
+
+	private static void put(Status s)
+	{
+		repo.Add (s.name, assignID (s));
+	}
+
+	private static Status assignID(Status s)
+	{
+		s._id = latestID++;
+		return s;
+	}
 	#endregion
 
 	#region INSTANCE_METHODS
 
+	public Status(string name, string desc, Sprite icon, DecayType dt, int stacksMax, float duration, params StatusComponent[] components)
+	{
+		this.name = name;
+		this.desc = desc;
+		this.icon = icon;
+		this.decayType = dt;
+		initDuration = duration;
+		_duration = initDuration;
+
+		this.stacksMax = stacksMax;
+		_stacks = 1;
+
+		this.components = components;
+	}
+	public Status(Status s) : this(s.name, s.desc, s.icon, s.decayType, s.stacksMax, s.initDuration, s.components) { }
+	public Status(Status s, float duration) : this(s)
+	{
+		initDuration = duration;
+		_duration = initDuration;
+	}
+
+	public float durationPercentage()
+	{
+		return _duration / initDuration;
+	}
+
+	#endregion
+
+	#region INTERNAL_TYPES
+
+	/// <summary>
+	/// The types of status decay behavior
+	/// </summary>
+	public enum DecayType { communal, serial }
 	#endregion
 }
