1d63ff1863ad4bdd83df7a487e84f57fce10c286
Kevin Adams <kadams8670@gmail.com>
2018-01-17 20:04:47 -0600

    Utility scripts
    Added utility scripts for Singleton and DontDestroyOnLoad

---
+++ Assets/Scripts/Utility/DontDestroyOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour 
+{
+	public static GameObject inst; 
+
+	void Awake() 
+	{
+		if (inst == null)
+		{
+			inst = this.gameObject; 
+			DontDestroyOnLoad(transform.gameObject);
+		}
+		else if (this.gameObject != inst)
+		{
+			Destroy(this.gameObject); 
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Parent class for singleton GameObjects
+/// Any class that should be marked as a singleton should inherit from this class using the declaration:
+/// 	ClassName : Singleton<ClassName> 
+/// Inheriting from this class prevents more than one instance of the class from being instantiated
+/// Once a class inherits from Singleton, its instance can be accessed from any script using the statement:
+/// 	ClassName.inst.STATEMENT
+/// </summary>
+
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+	private static T instance; 
+
+	// The variable used for accessing the instance of the singleton
+	public static T inst 
+	{ 
+		get 
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindObjectOfType<T>(); 
+			}
+			return instance; 
+		}
+	}
+}
+
+
+/*
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+	// The variable used for accessing the instance of the singleton
+	public static T inst { get; private set; }
+
+	// Called in Awake()
+	// There could be errors if other scripts try to call these singletons before CheckSingleton() has been called
+	// You should avoid calling any singleton instances until after Awake()
+	public void CheckSingleton()
+	{
+		if (inst == null)
+		{
+			inst = (T) this;
+		}
+		else
+		{
+			Debug.LogError("Got a second instance of the class " + this.GetType());
+		}
+	}
+
+	void Awake()
+	{
+		// This line must be included first
+		CheckSingleton(); 
+	}
+}
+*/
