93705549fcbbda7249642fec28cf1ceb3e1efc1a
Sam Streed <embersteelstreus@gmail.com>
2018-01-18 15:44:31 -0600

    Added camera profile system. Mostly fixed bounds limiting; still a little wonky in edge cases.

---
+++ Assets/Scripts/Camera/CameraProfile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewProfile", menuName = "Camera/Profile")]
+public class CameraProfile : ScriptableObject
+{
+	#region INSTANCE_VARS
+	public Vector3 position = Vector3.zero;
+	public Vector3 rotation = Vector3.zero;
+	public bool smoothFollow = false;
+	public float smoothSpeed = 5f;
+	public float followRadius = 0f;
+	public float zoomLevel = 5f;
+	#endregion
+}
