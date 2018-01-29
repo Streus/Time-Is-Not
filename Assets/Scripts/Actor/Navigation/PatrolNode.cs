a8e10156554bf3a824ae91e5aca6b16ed41da740
Sam Streed <embersteelstreus@gmail.com>
2018-01-28 21:43:49 -0600

    Added basic PatrolNode system for patrol AI.

---
+++ Assets/Scripts/Actor/Navigation/PatrolNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolNode : MonoBehaviour
+{
+	[SerializeField]
+	private PatrolNode next;
+
+	public void OnDrawGizmos()
+	{
+		if (next != null)
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine (transform.position, next.transform.position);
+		}
+	}
+}
