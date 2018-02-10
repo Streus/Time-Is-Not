6eacaca9b87e0bb5c41169fd5d2dc4f5bf770bee
Kevin Adams <kadams8670@gmail.com>
2018-02-10 11:50:59 -0600

    Fixed Player Input Code Not Compiling

--- Assets/Scripts/Actor/Control/Actions/PlayerDefaultControl.cs
+++ Assets/Scripts/Actor/Control/Actions/PlayerDefaultControl.cs
@@ -12,18 +12,18 @@ public class PlayerDefaultControl : Action
 		Player p = State.cast<Player> (c);
 
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
-		if (Input.GetKeyDown (PlayerControlManager.RH_FireStasis || PlayerControlManager.LH_FireStasis))
+		if (Input.GetKeyDown (PlayerControlManager.RH_FireStasis) || Input.GetKeyDown(PlayerControlManager.LH_FireStasis))
 			p.getSelf ().getAbility (0).use (p.getSelf (), mousePos);
 
 		Vector2 movementVector = Vector2.zero;
 
-		if(Input.GetKey(PlayerControlManager.RH_Up || PlayerControlManager.LH_UP)) // UP
+		if(Input.GetKey(PlayerControlManager.RH_Up) || Input.GetKey(PlayerControlManager.LH_UP)) // UP
 			movementVector += Vector2.up;
-		if (Input.GetKey (PlayerControlManager.RH_Left || PlayerControlManager.LH_Left)) // LEFT
+		if (Input.GetKey (PlayerControlManager.RH_Left) || Input.GetKey (PlayerControlManager.LH_Left)) // LEFT
 			movementVector += Vector2.left;
-		if(Input.GetKey(PlayerControlManager.RH_Down || PlayerControlManager.LH_Down)) // DOWN
+		if(Input.GetKey(PlayerControlManager.RH_Down) || Input.GetKey(PlayerControlManager.LH_Down)) // DOWN
 			movementVector += Vector2.down;
-		if (Input.GetKey (PlayerControlManager.RH_Right || PlayerControlManager.LH_Right)) // RIGHT
+		if (Input.GetKey (PlayerControlManager.RH_Right) || Input.GetKey(PlayerControlManager.LH_Right)) // RIGHT
 			movementVector += Vector2.right;
 
 		movementVector = movementVector.normalized * p.getSelf().getMovespeed () * Time.deltaTime;
