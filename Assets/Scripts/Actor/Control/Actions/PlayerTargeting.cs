f70c7ad116bdd7f96f710bd9492d50936e654f9c
esantos147 <esantos@mail.bradley.edu>
2018-02-13 10:42:41 -0600

    Added the needed scripts for dash implementation

---
+++ Assets/Scripts/Actor/Control/Actions/PlayerTargeting.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Actions/Player/Targeting")]
+public class PlayerTargeting : Action
+{
+    public override void perform(Controller c)
+    {
+       
+    }
+}
