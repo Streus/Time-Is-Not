f047eafd5a6238cc2fd916aa3079010213e56b7e
Stephan Cantero <scantero@mail.bradley.edu>
2018-02-14 01:54:20 -0600

    Added temporary animation controller for Margaux + door animation test
    Added temporary animation controller script and tested horizontal door animation.

---
+++ Assets/TempPlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerScript : MonoBehaviour {
+
+	private Animator _animationController;
+
+	void Start () {
+		_animationController = GetComponent<Animator> ();
+	}
+
+	void Update () {
+//		print ();
+
+		_animationController.SetFloat ("xSpeed", Input.GetAxis ("Horizontal"));
+		_animationController.SetFloat ("ySpeed", Input.GetAxis ("Vertical"));
+//		_animationController.SetTrigger ("ActivateTimeTether");
+//		_animationController.SetTrigger ("Reappear");
+	}
+}
