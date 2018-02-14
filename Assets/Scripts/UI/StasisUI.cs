151b937050afa2318c0369dc83bf393d62c9a2e9
Kevin Adams <kadams8670@gmail.com>
2018-02-13 22:33:39 -0600

    Added UI Art Assets
    Updated UI art assets using proper dimensions

--- Assets/Scripts/UI/StasisUI.cs
+++ Assets/Scripts/UI/StasisUI.cs
@@ -5,6 +5,17 @@ using UnityEngine.UI;
 public class StasisUI : MonoBehaviour
 {
 	[SerializeField] private int stasisUiNum;
+
+	public Sprite onSprite;
+	public Sprite offSprite; 
+
+	Image image; 
+
+	void Start()
+	{
+		image = GetComponent<Image>(); 
+	}
+
 	void Update()
 	{
 		numStasis(stasisUiNum);
@@ -13,11 +24,13 @@ public class StasisUI : MonoBehaviour
 	{
 		if(LevelStateManager.numStasis >= stasisUiNum)
 		{
-			this.GetComponent<Image>().CrossFadeAlpha(0.15f, 0.25f, true);
+			//this.GetComponent<Image>().CrossFadeAlpha(0.15f, 0.25f, true);
+			image.sprite = offSprite; 
 		}
 		else
 		{
-			this.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.25f, true);
+			//this.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.25f, true);
+			image.sprite = onSprite; 
 		}
 	}
 }
---
+++ Assets/Scripts/UI/StasisUIPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StasisUIPanel : MonoBehaviour 
+{
+	public GameObject stasisPanel; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		UpdateStasisPanelActive(); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// TODO UpdateStasisPanelActive() should probably be called when the stasis ability is collected
+	}
+
+	public void UpdateStasisPanelActive()
+	{
+		if (GameManager.inst.canUseStasis)
+		{
+			stasisPanel.SetActive(true); 
+		}
+		else
+		{
+			stasisPanel.SetActive(false); 
+		}
+	}
+}
