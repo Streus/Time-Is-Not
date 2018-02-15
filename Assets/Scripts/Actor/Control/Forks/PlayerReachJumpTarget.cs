f3f93a48c4ce1c3917221a0e4b9e99163f6dc906
Sam Streed <embersteelstreus@gmail.com>
2018-02-15 09:59:52 -0600

    Dash cooldown now pauses whenin dash state.

--- Assets/Scripts/Actor/Control/Forks/PlayerReachJumpTarget.cs
+++ Assets/Scripts/Actor/Control/Forks/PlayerReachJumpTarget.cs
@@ -9,9 +9,12 @@ public class PlayerReachJumpTarget : Fork
     {
         Player p = State.cast<Player>(c);
 
+		p.getSelf ().getAbility (1).active = false;
+
 		if (Vector2.Distance (p.transform.position, p.getJumpTargetPos) < 0.1f)
 		{
-			p.gameObject.layer = 8;
+			p.gameObject.layer = LayerMask.NameToLayer("GroundEnts");
+			p.getSelf ().getAbility (1).active = true;
 			return true;
 		}
 		return false;
