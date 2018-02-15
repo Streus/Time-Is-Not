b52216005d1a8288d4662eafec459c588adc8720
Sam Streed <embersteelstreus@gmail.com>
2018-02-14 21:14:17 -0600

    Player can no longer jump through lasers without being affected.

--- Assets/Scripts/Actor/Control/Actions/PlayerDashing.cs
+++ Assets/Scripts/Actor/Control/Actions/PlayerDashing.cs
@@ -10,7 +10,7 @@ public class PlayerDashing : Action
     public override void perform(Controller c)
     {
         Player p = State.cast<Player>(c);
-        p.gameObject.layer = 9;
+		p.gameObject.layer = LayerMask.NameToLayer("MPPassenger");
 		p.transform.position = Vector2.Lerp(p.transform.position, p.getJumpTargetPos, dashSpeed * Time.deltaTime);
     }
 }
