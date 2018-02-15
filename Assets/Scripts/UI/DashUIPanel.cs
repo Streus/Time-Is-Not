45e9e06c77ae13cc6d1baff89b1fba8ac5ec0aa5
Kevin Adams <kadams8670@gmail.com>
2018-02-14 20:53:35 -0600

    CanUseDash Hooked up to UI

---
+++ Assets/Scripts/UI/DashUIPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashUIPanel : MonoBehaviour 
+{
+	[Tooltip("(Drag In) The dash UI piece of the time tether that displays dash UI")]
+	public GameObject dashPanel; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		UpdateDashPanelActive(); 
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// TODO UpdateDashPanelActive() should probably be called when the dash ability is collected
+	}
+
+	public void UpdateDashPanelActive()
+	{
+		if (GameManager.inst.canUseDash)
+		{
+			dashPanel.SetActive(true); 
+		}
+		else
+		{
+			dashPanel.SetActive(false); 
+		}
+	}
+}
