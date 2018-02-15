cd308a352b6a77d0819f6a9821a9f60ceef73bce
Sam Streed <embersteelstreus@gmail.com>
2018-02-14 20:14:21 -0600

    Added more checks for shooting SB. That's right: MORE CHECKS AHAHAHAHAHA.

--- Assets/Scripts/Actor/Control/Actions/PlayerDefaultControl.cs
+++ Assets/Scripts/Actor/Control/Actions/PlayerDefaultControl.cs
@@ -11,10 +11,14 @@ public class PlayerDefaultControl : Action
 
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
 
+		bool inSBBounds = LevelStateManager.StasisBubbleAtPos (mousePos);
+
 		// Use Stasis Placement ablility
 		if ((Input.GetKeyDown (PlayerControlManager.RH_FireStasis) ||
 			Input.GetKeyDown(PlayerControlManager.LH_FireStasis)) && 
-			GameManager.inst.canUseStasis)
+			GameManager.inst.canUseStasis && 
+			!inSBBounds &&
+			GameManager.CursorInGameplayState())
 			p.getSelf ().getAbility (0).use (p.getSelf (), mousePos);
 
         p.move();
