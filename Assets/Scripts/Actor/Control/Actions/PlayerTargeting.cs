afcd0d2cb6ff647b6bcd898ef256fce18ee36480
Sam Streed <embersteelstreus@gmail.com>
2018-02-14 20:34:41 -0600

    Broken Dash, fixed it. mbmb.

--- Assets/Scripts/Actor/Control/Actions/PlayerTargeting.cs
+++ Assets/Scripts/Actor/Control/Actions/PlayerTargeting.cs
@@ -42,7 +42,8 @@ public class PlayerTargeting : Action
         else //mouse is within the max distance, no need to limit
             p.setJumpTargetPos(mp);
 
-		if(Input.GetKeyUp(PlayerControlManager.LH_Dash) || Input.GetKeyUp(PlayerControlManager.RH_Dash))
+		//TODO figure out how to get this working
+//		if(Input.GetKeyUp(PlayerControlManager.LH_Dash) || Input.GetKeyUp(PlayerControlManager.RH_Dash))
 			p.getSelf().getAbility(1).use(p.getSelf(), p.getJumpTargetPos);            
     }
 }
