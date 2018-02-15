597555eb33e7bc62034959381c00977b7579e15c
Stephan Cantero <scantero@mail.bradley.edu>
2018-02-15 08:59:19 -0600

    Updated Humming bird prefab
    Updated Hummingbird prefab

--- Assets/Scripts/Actor/Control/Controllers/Hummingbird.cs
+++ Assets/Scripts/Actor/Control/Controllers/Hummingbird.cs
@@ -24,12 +24,18 @@ public class Hummingbird : Controller
 
 	private Transform pursuitTarget;
 
+	private Animator _anim;
+	private Rigidbody2D _rigidBody;
+
 	#endregion
 
 	#region INSTANCE_METHODS
 
 	public override void Awake()
 	{
+		_anim = GetComponent<Animator> ();
+		_rigidBody = GetComponent<Rigidbody2D> ();
+
 		base.Awake ();
 
 		defaultState = getState();
