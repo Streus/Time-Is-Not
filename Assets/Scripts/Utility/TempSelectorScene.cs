a0e1da422957f286f0405bc16cbfb8e84ee8e53b
Kevin Adams <kadams8670@gmail.com>
2018-02-22 14:45:24 -0600

    Temp Scene Selector Splash Scene
    Added temp scene with buttons for choosing which scene to play

---
+++ Assets/Scripts/Utility/TempSelectorScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class TempSelectorScene : MonoBehaviour 
+{
+	public void LoadScene(string name)
+	{
+		if (SceneManager.GetSceneByName(name) != null)
+		{
+			SceneManager.LoadScene(name); 
+		}
+		else
+		{
+			Debug.LogError("Scene name " + name + " cannot be loaded"); 
+		}
+	}
+}
