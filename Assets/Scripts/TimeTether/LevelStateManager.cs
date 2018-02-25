3cdb9e6ac2dc15e66eb2f67ddc493e89a0187579
esantos147 <esantos@mail.bradley.edu>
2018-02-25 14:08:36 -0600

    removed janky stasis error sound

--- Assets/Scripts/TimeTether/LevelStateManager.cs
+++ Assets/Scripts/TimeTether/LevelStateManager.cs
@@ -341,7 +341,7 @@ public class LevelStateManager : Singleton<LevelStateManager>
 		}
         else
         {
-            AudioLibrary.PlayStasisErrorSound();
+            //AudioLibrary.PlayStasisErrorSound();
             return false;
         }
 	}
