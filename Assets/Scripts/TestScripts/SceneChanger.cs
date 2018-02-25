b8177e8fe3b4161df27353ce2219a52641324c9c
The-Alhooness <sarahkmcglasson@gmail.com>
2018-02-25 17:00:01 -0600

    Timeline cutscene stuff
    test cutscenes

---
+++ Assets/Scripts/TestScripts/SceneChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour 
+{
+	public string sceneToLoad;
+	bool changeAfterTimer = true;
+	float timer = 3.45f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(changeAfterTimer)
+			 StartCoroutine (Load (timer));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col) 
+	{
+		if (col.CompareTag ("Player"))
+			StartCoroutine(Load (0.5f));
+	}
+
+
+	public IEnumerator Load(float time)
+	{
+		yield return new WaitForSeconds (time);
+		SceneManager.LoadScene (sceneToLoad);
+	}
+}
