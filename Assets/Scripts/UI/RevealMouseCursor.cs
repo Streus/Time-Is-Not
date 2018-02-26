af1e5158e333d4138db7a9a8c65494da191d60e7
Kevin Adams <kadams8670@gmail.com>
2018-02-26 17:44:46 -0600

    Temp Fix for Mouse Cursor Hidden Bug
    Added a temp script to TempSelectorScene that ensures the mouse cursor isn't hidden

---
+++ Assets/Scripts/UI/RevealMouseCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealMouseCursor : MonoBehaviour 
+{
+	public bool revealOnStart; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (revealOnStart)
+		{
+			Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+			Cursor.visible = true;
+		}
+	}
+}
