e58816e373b6942fe97007118d31e1b8758f0aa2
The-Alhooness <sarahkmcglasson@gmail.com>
2018-03-01 11:29:46 -0600

    Laser and door animation tweaks

--- Assets/Scripts/Game/SpriteOrderer.cs
+++ Assets/Scripts/Game/SpriteOrderer.cs
@@ -11,13 +11,13 @@ public class SpriteOrderer : Singleton<SpriteOrderer>
 
 		for(int i = 0; i < sprites.Length; i ++)
 		{
-			sprites [i].sortingOrder =  (int)Mathf.Round (sprites [i].transform.position.y * 10) * -1;
+			sprites [i].sortingOrder =  (int)Mathf.Round (sprites [i].transform.position.y * 100) * -1;
 		}
 	}
 
 	public int OrderMe(Transform trans)
 	{
-		int layer = -1 * (int)Mathf.Round (trans.position.y * 10);
+		int layer = -1 * (int)Mathf.Round (trans.position.y * 100);
 			
 		return layer;
 	}
