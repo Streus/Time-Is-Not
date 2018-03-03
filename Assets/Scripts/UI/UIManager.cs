bccbf00e5d9226b8db3eba3231ce249a4f9b8216
esantos147 <esantos@mail.bradley.edu>
2018-03-03 14:25:50 -0600

    base main menu

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SaveManager.Load();
+        SceneManager.LoadScene(SaveManager.level);
+    }
+
+    public void NewGame()
+    {
+        SaveManager.NewGame();
+        SceneManager.LoadScene(SaveManager.level);
+    }
+
+    public void OpenPanel(GameObject panelToOpen)
+    {
+        panelToOpen.SetActive(true);
+    }
+
+    public void ClosePanel(GameObject panelToClose)
+    {
+        panelToClose.SetActive(false);
+    }
+}
