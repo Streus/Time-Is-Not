cbf541637ba39248fcbd1fb1b5cf3b9f3f93c274
esantos147 <esantos@mail.bradley.edu>
2018-03-05 20:29:27 -0600

    Updated slider positions to go across scenes

---
+++ Assets/Scripts/UI/UpdateMasterVolumeSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateMasterVolumeSlider : MonoBehaviour
+{
+
+    Slider masterSlider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        masterSlider = this.GetComponent<Slider>();
+        masterSlider.value = (int)(UIManager.inst.GetMasterVolume());
+    }
+}
