f27519ff47d2d014e033b0b3f3c5d8997be1791f
esantos147 <esantos@mail.bradley.edu>
2018-03-06 10:45:38 -0600

    Added Ambient sound slider and option

---
+++ Assets/Scripts/UI/UpdateAmbientVolumeUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateAmbientVolumeUI : MonoBehaviour
+{
+    Slider ambientVolumeSlider;
+    Text ambientVolumeText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ambientVolumeSlider = this.GetComponentInChildren<Slider>();
+        ambientVolumeText = this.GetComponentInChildren<Text>();
+        ambientVolumeSlider.value = (int)(UIManager.inst.GetAmbientVolume());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ambientVolumeText.text = "Ambient Volume: " + (int)(((UIManager.inst.GetAmbientVolume() - -80) / (0 - -80)) * 100);
+    }
+}
