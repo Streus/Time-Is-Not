d16fc01dd616e5286fa21a37c4d7bcd2486d83a9
The-Alhooness <sarahkmcglasson@gmail.com>
2018-03-22 11:25:13 -0500

    zach final level stuff

---
+++ Assets/Scripts/Game/InteractablesActivatables/TimerTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour, IActivatable
+{
+	[SerializeField]
+	private float timerDurationSeconds;
+
+	/// <summary>
+	/// Toggles the laser's state and returns it.
+	/// </summary>
+	public bool onActivate()
+	{
+		GameManager.inst.StartEndTimer (timerDurationSeconds);
+		return true;
+	}
+
+	/// <summary>
+	/// Sets the laser's state and returns it.
+	/// </summary>
+	public bool onActivate(bool state)
+	{
+		GameManager.inst.StartEndTimer (timerDurationSeconds);
+		return true;
+	}
+
+
+}
