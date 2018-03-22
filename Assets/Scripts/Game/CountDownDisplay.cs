f786461433405c27acb07eb0b5808c12f26cd0fb
The-Alhooness <sarahkmcglasson@gmail.com>
2018-03-22 11:15:01 -0500

    CountdownDisplay

---
+++ Assets/Scripts/Game/CountDownDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountDownDisplay : MonoBehaviour 
+{
+	private Text screenText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		screenText = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (GameManager.inst.useEndTimer)
+			screenText.text = GameManager.inst.timerString;
+		else
+			screenText.text = "";
+
+	}
+}
