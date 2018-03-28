61e3b5a7f1d5c69330ac3370fe6c8ef90050ac24
The-Alhooness <sarahkmcglasson@gmail.com>
2018-03-28 14:53:09 -0500

    meltdown event

---
+++ Assets/Scripts/Game/MeltdownController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeltdownController : MonoBehaviour, IActivatable
+{
+	[SerializeField]
+	private int _state;
+
+	public bool onActivate()
+	{
+		_state++;
+		return true;
+	}
+
+	public bool onActivate (bool state)
+	{
+		_state++;
+		return true;
+	}
+
+	public int State
+	{
+		get{return _state;}
+	}
+}
---
+++ Assets/Scripts/Game/MeltdownDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeltdownDisplay : MonoBehaviour 
+{
+	[SerializeField]
+	private Sprite[] _sprites;
+
+	private SpriteRenderer _rend;
+
+	[SerializeField]
+	private MeltdownController _controller;
+
+
+	// Use this for initialization
+	void Awake () 
+	{
+		_rend = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(_controller == null)
+		{
+			Debug.LogError ("Error: no controller.");
+			return;
+		}
+		if(_rend == null)
+		{
+			Debug.LogError ("Error: no sprite renderer on display object.");
+			return;
+		}
+		if(_controller.State >= _sprites.Length || _controller.State < 0)
+		{
+			Debug.LogError ("Error: controller state out of bounds.");
+			return;
+		}
+		_rend.sprite = _sprites [_controller.State];
+	}
+}
