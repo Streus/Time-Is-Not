cd00c7bfe91c0b826d454100292b0efa39b21d78
zabbott <zabbott@mail.bradley.edu>
2018-03-28 22:44:25 -0500

    Level 6 changes

--- Assets/Scripts/Game/EndTimerTrigger.cs
+++ Assets/Scripts/Game/EndTimerTrigger.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class EndTimerTrigger : MonoBehaviour {
-    public Text[] WorldCanvses; 
+    public Text[] WorldCanvses;
+    public bool TurnOnTimers; 
 	// Use this for initialization
 	void Start () {
 		
@@ -12,15 +13,16 @@ public class EndTimerTrigger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(TurnOnTimers == true){
+            TurnOnTexts();
+        }
 	}
 
     public void OnTriggerEnter2D(Collider2D other){ 
         if(other.gameObject.tag == "Player"){ 
             if(GameManager.inst.useEndTimer == false){ 
                 GameManager.inst.StartEndTimer();
-
-
+                TurnOnTimers = true;
             }
         }
     }
@@ -28,7 +30,11 @@ public class EndTimerTrigger : MonoBehaviour {
     public void TurnOnTexts() 
     {
         for (int i = 0; i < WorldCanvses.Length; i++){
-            WorldCanvses[i].enabled = true;
+            if (WorldCanvses[i].enabled == false)
+            {
+                WorldCanvses[i].enabled = true;
+            }
         }
+        TurnOnTimers = false;
     }
 }
