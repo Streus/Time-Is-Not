7b0073796b1f23f08b455a80ec2446c1294f9a35
zabbott <zabbott@mail.bradley.edu>
2018-03-28 21:57:31 -0500

    Level 6 updates

---
+++ Assets/Scripts/Game/EndTimerTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTimerTrigger : MonoBehaviour {
+    public Text[] WorldCanvses; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D other){ 
+        if(other.gameObject.tag == "Player"){ 
+            if(GameManager.inst.useEndTimer == false){ 
+                GameManager.inst.StartEndTimer();
+
+
+            }
+        }
+    }
+
+    public void TurnOnTexts() 
+    {
+        for (int i = 0; i < WorldCanvses.Length; i++){
+            WorldCanvses[i].enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/TimerText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class TimerText : MonoBehaviour {
+    private Text myText; 
+	// Use this for initialization
+	void Start () {
+        myText = GetComponent<Text>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myText.text = GameManager.inst.timerString;
+	}
+}
