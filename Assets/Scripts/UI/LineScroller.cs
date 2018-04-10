89560854c4ff629fac1bff7b212927bda0ff031e
The-Alhooness <sarahkmcglasson@gmail.com>
2018-04-10 16:47:21 -0500

    Line Scroller and security laser effect assets

---
+++ Assets/Scripts/UI/LineScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineScroller : MonoBehaviour 
+{
+	private LineRenderer _line;
+	private Material _mat;
+	[SerializeField]
+	private float _speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_line = gameObject.GetComponent<LineRenderer> ();
+		_mat = new Material (_line.material);
+		_line.material = _mat;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		_line.material.mainTextureOffset += new Vector2 (Time.deltaTime * _speed, 0); 
+		
+	}
+}
