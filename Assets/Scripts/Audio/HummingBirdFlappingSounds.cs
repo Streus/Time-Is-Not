5a48835dc2dd61dd7c5614e4b244b0de2b721aaf
atholt95 <32171853+atholt95@users.noreply.github.com>
2018-04-17 11:13:17 -0500

    Added Hummingbird flapping sounds
    Added Hummingbird flapping sounds

--- Assets/Scripts/Audio/HummingBirdFlappingSounds.cs
+++ Assets/Scripts/Audio/HummingBirdFlappingSounds.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HummingBirdFlappingSounds : MonoBehaviour {
-    AudioSource source;
+    public AudioSource source;
     // Use this for initialization
     void Start () {
         source = this.GetComponentInParent<AudioSource>();
