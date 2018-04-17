38298abaeaf7c74f027a218c3c65d4765fa88064
Kevin Adams <kadams8670@gmail.com>
2018-04-16 21:56:57 -0500

    Added script for tether menu sounds

---
+++ Assets/Scripts/TimeTether/TetherMenuSounds.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetherMenuSounds : MonoBehaviour 
+{
+
+	public void PlayTetherMenuInSound()
+	{
+		Debug.Log("Play tether menu in sound"); 
+	}
+
+	public void PlayTetherMenuOutSound()
+	{
+		Debug.Log("Play tether menu out sound"); 
+	}
+}
