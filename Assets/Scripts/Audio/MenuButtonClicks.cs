573199130c496bdbb2c79176cbc0cc906e4d909e
esantos147 <esantos@mail.bradley.edu>
2018-04-20 18:00:21 -0500

    menu button sounds

---
+++ Assets/Scripts/Audio/MenuButtonClicks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonClicks : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioSource source;
+
+    private void Awake()
+    {
+        source = this.gameObject.GetComponent<AudioSource>();
+        source.clip = clip;
+    }
+
+    public void PlayMenuButton()
+    {  
+        source.Play();
+    }
+}
