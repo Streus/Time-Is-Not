5700baf3d85a8ec1cee9f503ef4bfefe0e2857f0
Kevin Adams <kadams8670@gmail.com>
2018-04-24 11:11:58 -0500

    Teleporter Script / Effect

---
+++ Assets/Scripts/World/TeleporterPad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterPad : MonoBehaviour 
+{
+	//[SerializeField] ParticleSystem idleParticles; 
+	[SerializeField] ParticleSystem teleportParticles; 
+
+	public void OnStartTeleport()
+	{
+		teleportParticles.Play(); 
+	}
+}
