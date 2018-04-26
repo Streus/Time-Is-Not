e7d3cc91d41e412a994a71d81a0c0d91e9a5a885
zabbott <zabbott@mail.bradley.edu>
2018-04-25 21:03:26 -0500

    Credits

---
+++ Assets/CreditsAnims/CredsNav.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables; 
+
+public class CredsNav : MonoBehaviour {
+    public float Timer;
+    public PlayableDirector myDirector;
+    public GameObject[] DestroyThemAll; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime; 
+        if(myDirector.time > 55f) {
+            for (int i = 0; i < DestroyThemAll.Length; i++){
+                Destroy(DestroyThemAll[i]);
+            }
+            myDirector.Pause();
+
+        
+        }
+	}
+
+    public void LoadScene(string name){ 
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Assets/CreditsAnims/MoveTowards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour {
+    public Transform StartPos;
+    public Transform End; 
+    public float speed; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, End.position, speed * Time.deltaTime);
+	}
+}
